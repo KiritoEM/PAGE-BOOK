c5924922d0fbc0fb4e5bc1e6a8a70423848659e2
kiritoEM <johankirito64@gmail.com>
2023-12-09 16:02:43 +0100

    add responsive tablet

--- src/components/Welcoming.tsx
+++ src/components/Welcoming.tsx
@@ -3,11 +3,11 @@ import WelcomingHeader from "./welcoming-page/WelcomingHeader";
 const Welcoming = (): JSX.Element => {
   return (
     <section id="welcoming-page">
-      <div className="row">
-        <div className="col-6">
+      <div className="row gy-5 gy-md-2">
+        <div className="col-md-7 col-lg-6">
           <WelcomingHeader />
         </div>
-        <div className="col-6">
+        <div className="col-md-5 col-lg-6">
             <div className="section-cover">
                 <img src="/image/Book.png" alt="" />
             </div>
