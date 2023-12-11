c9266de9792cebb5730f44c91dbc887b185f0a06
kiritoEM <johankirito64@gmail.com>
2023-12-11 07:09:57 +0100

    add card to trusted-section

--- src/components/Trusted/TrustedCard.tsx
+++ src/components/Trusted/TrustedCard.tsx
@@ -1,12 +1,15 @@
-const TrustedCard = () : JSX.Element => {
-    return (
-        <div className="trusted-card">
-             <div className="trusted-card__img">
-                 <img src="/image/trusted-1.png" alt="" />
-             </div>
-             <div className="truested-card__content"></div>
-        </div>
-    );
+const TrustedCard = (): JSX.Element => {
+  return (
+    <div className="trusted-card">
+      <div className="trusted-card__img">
+        <img src="/image/trusted-1.png" alt="" />
+      </div>
+      <div className="trusted-card__content mt-4">
+        <h5>Amazen Corp</h5>
+        <p>A long established fact that a who looking at its layout.</p>
+      </div>
+    </div>
+  );
 };
 
 export default TrustedCard;
