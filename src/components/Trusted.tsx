ece6654f089d7c69c8c82581796700a27afc56d5
kiritoEM <johankirito64@gmail.com>
2023-12-11 07:06:40 +0100

    add header

--- src/components/Trusted.tsx
+++ src/components/Trusted.tsx
@@ -1,9 +1,18 @@
+import TrustedCard from "./Trusted/TrustedCard";
+
 const Trusted = (): JSX.Element => {
   return (
     <section id="trusted">
       <div className="section-header">
         <h3>Author’s Book Includes</h3>
         <div className="line"></div>
+      </div>
+      <div className="section-content">
+        <div className="row">
+          <div className="col-3">
+            <TrustedCard />
+          </div>
+        </div>
       </div>
     </section>
   );
---
+++ src/components/Trusted/TrustedCard.tsx
@@ -0,0 +1,12 @@
+const TrustedCard = () : JSX.Element => {
+    return (
+        <div className="trusted-card">
+             <div className="trusted-card__img">
+                 <img src="/image/trusted-1.png" alt="" />
+             </div>
+             <div className="truested-card__content"></div>
+        </div>
+    );
+};
+
+export default TrustedCard;
