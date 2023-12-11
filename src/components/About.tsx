c39702d8f118a040a210016deb18ee0bb71dfaae
kiritoEM <johankirito64@gmail.com>
2023-12-11 05:49:09 +0100

    add responsive to about

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -3,13 +3,13 @@ import AboutContent from "./about/AboutContent";
 const About = (): JSX.Element => {
   return (
     <section id="about">
-      <div className="row gx-5">
-        <div className="col-5">
+      <div className="row gx-lg-2 gy-5 gy-md-2">
+        <div className="col-md-5">
           <div className="section-cover">
             <img src="/image/about.png" alt="" />
           </div>
         </div>
-        <div className="col-7">
+        <div className="col-md-7">
           <AboutContent />
         </div>
       </div>
